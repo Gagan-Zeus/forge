7bdb90dfb6871adaa362f03361a7a2209fe83c19
Gagan Deep S <ugcet2300113@reva.edu.in>
2026-04-22 12:15:47 +0530

    refactor: update logging level to WARNING in run.py for improved log management

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from bot.handlers import run_bot
 
 load_dotenv()
-logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"), format="%(asctime)s %(levelname)s %(name)s: %(message)s")
+logging.basicConfig(level=os.getenv("LOG_LEVEL", "WARNING"), format="%(asctime)s %(levelname)s %(name)s: %(message)s")
 
 if __name__ == "__main__":
     run_bot(
